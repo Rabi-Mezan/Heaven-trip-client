a79a043d06359abcdd29091d8ba50a855a523768
Rabi-Mezan <ravimezan@gmail.com>
2021-10-31 00:23:39 +0600

    about us added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Banner from '../Banner/Banner';
 import Story from '../Home/Story/Story';
 import './About.css'
 
 const About = () => {
     return (
         <div>
-            <div>
-
+            <div className='about-banner h-96'>
+                <h1 className='text-4xl font-bold text-white'>About Us</h1>
             </div>
             <Story></Story>
         </div>
