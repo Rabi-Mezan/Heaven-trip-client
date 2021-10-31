44c4ecc453f393f9bec3cc80a664c2c405b6af50
Rabi-Mezan <ravimezan@gmail.com>
2021-10-31 21:24:33 +0600

    final commit

--- src/Pages/BookTrip/BookTrip.js
+++ src/Pages/BookTrip/BookTrip.js
@@ -44,12 +44,12 @@ const BookTrip = () => {
             <div className='lg:w-2/4 trip-details flex flex-col justify-center items-center'>
                 <img className='w-80 h-80 p-5 border-2 border-gray-500 lg:rounded-full mb-5' src={trip.img} alt="" />
                 <p className='flex justify-between text-xl font-bold'>{trip.title} <span className='ml-10 text-yellow-400'>${trip.price}</span></p>
-                <p className='text-sm text-center w-2/3 mt-2'>{trip.description}</p>
+                <p className='text-sm text-center lg:w-2/3 mt-2'>{trip.description}</p>
 
             </div>
             <div className='book-trip  lg:w-2/4 p-12'>
                 <p className='text-2xl font-bold text-center text-gray-400'>{user.displayName} </p> <br />
-                <p className='text-gray-400 font-bold lg:text-2xl text-xl mb-5  text-center'> You are almost ready to book <br /> our <span className='Font-bold text-3xl text-yellow-400'> {trip.title} </span>tour package !!</p>
+                <p className='text-gray-400 font-bold lg:text-2xl text-xl mb-5  text-center'> You are  ready to book <br /> our <span className='Font-bold lg:text-3xl text-yellow-400'> {trip.title} </span>tour package !!</p>
                 <div>
                     <div className='mt-8'>
                         <form className='book lg:w-2/3 m-auto' onSubmit={handleSubmit(onSubmit)}>
